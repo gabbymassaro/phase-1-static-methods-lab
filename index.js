d3ab4e2c0aacef1b3dd553c85b141342473e166c
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-05 17:33:23 -0400

    Titleize passes

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ class Formatter {
   static sanitize(string) {
     return string.replace(/[^A-Za-z0-9-' ]+/g, "", /\s/)
   }
+  static titleize(string) {
+    let title = ""
+    const dontCapitalize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    string.split(" ").forEach((word, index) => {
+      if (dontCapitalize.includes(word) && index !== 0) {
+        title += word + " "
+      } else {
+        const capitalize = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        title += capitalize + " "
+      }
+    })
+    return title.trim()
+  }
 }
